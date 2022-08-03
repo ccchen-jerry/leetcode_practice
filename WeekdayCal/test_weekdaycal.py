6b4911346fdc42ee84f912b73e5821c31e51f0e2
Chi-Chung Chen <npc4sell@gmail.com>
2022-08-03 16:28:38 +0800

    extend test cases to 1000

--- WeekdayCal/test_weekdaycal.py
+++ WeekdayCal/test_weekdaycal.py
@@ -3,7 +3,7 @@ import datetime, calendar
 from weekdaycal import day_of_week
 
 def test_date():
-    for i in range(10):
+    for i in range(1000):
         random_day = datetime.date.fromtimestamp(random.randint(1, 1000000000))
         print(str(random_day))
         assert day_of_week(random_day.year, random_day.month, random_day.day) == calendar.day_name[random_day.weekday()]
