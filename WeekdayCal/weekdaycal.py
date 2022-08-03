9abbe9482a6767a0407a344506a628f60fb35165
Chi-Chung Chen <npc4sell@gmail.com>
2022-08-03 16:18:35 +0800

    done algorithm

--- WeekdayCal/weekdaycal.py
+++ WeekdayCal/weekdaycal.py
@@ -4,6 +4,17 @@ Using Zeller's congruence
 
 '''
 
-WEEKDAY = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-MONTH = ["January", "February", "March", "April", "May", "June",
-         "July", "August", "September", "October", "November", "December"]
+Zeller_WEEKDAY = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+def day_of_week(year:int, month:int, day:int):    
+    #converting starting month of the year
+    if month < 3:
+        month += 12
+        year -= 1
+    
+    #creating necessary variables
+    centry = year // 100
+
+    weekdaynum = (day + (13 * (month + 1)) // 5 + year + year // 4 - year // 100 + year // 400) % 7
+
+    return Zeller_WEEKDAY[weekdaynum]
