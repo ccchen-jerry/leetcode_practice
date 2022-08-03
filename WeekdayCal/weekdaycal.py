77befb5d06deedfc37663933a6f92dfa40639e1f
Chi-Chung Chen <npc4sell@gmail.com>
2022-08-03 11:23:48 +0800

    create file

---
+++ WeekdayCal/weekdaycal.py
@@ -0,0 +1,9 @@
+'''
+Using Zeller's congruence
+
+
+'''
+
+WEEKDAY = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+MONTH = ["January", "February", "March", "April", "May", "June",
+         "July", "August", "September", "October", "November", "December"]
