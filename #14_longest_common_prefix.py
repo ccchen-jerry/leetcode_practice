9da933707567c59717100209d3bb7362a05348e8
Chi-Chung Chen <74597316+cccjry@users.noreply.github.com>
2022-12-25 21:35:36 +0800

    #14 task complete

--- #14_longest_common_prefix.py
+++ #14_longest_common_prefix.py
@@ -12,3 +12,49 @@ class Solution(object):
 # testcase2: ["dog","racecar","car"]
 
 '''
+
+# %%
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        # initial result
+        final_result = ""
+        # sorting lists
+        strs.sort()
+
+        if len(strs) == 0:
+            return final_result
+
+        if len(strs) == 1:
+            return strs[0]
+
+        # iterate from second word of the list 
+        for s in strs[1:]:
+            # initial before every comparison
+            result = ""
+            # only check within the length of the first word
+            for i in range(len(strs[0])):
+                if strs[0][i] == s[i]:
+                    result += strs[0][i]
+                else:
+                    break
+            final_result = result
+        
+        return final_result
+            
+            
+
+
+#%%
+strs = ["dog","racecar","car"]
+strs.sort()
+print(strs)
+print(len(strs))
+
+test1 = Solution()
+print("result = " , test1.longestCommonPrefix(strs))
+
+# %%
